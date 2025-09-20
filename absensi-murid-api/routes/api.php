fbd420eeb1e8952e426aa8e02a5399588573248a
RizkyFirman00 <firmansyah.mrf@gmail.com>
2025-09-20 10:51:02 +0700

    Add: User sanctum authorization
    Sampe sini route auth udah berhasil

--- absensi-murid-api/routes/api.php
+++ absensi-murid-api/routes/api.php
@@ -4,6 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 // Auth
-Route::post('/login',[AuthController::class, 'login']);
-Route::post('/logout',[AuthController::class, 'logout'])->middleware('auth:sanctum');
-Route::post('/register',[AuthController::class, 'register']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/profile', function (\Illuminate\Http\Request $request) {
+        return $request->user();
+    });
+});
